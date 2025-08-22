20c923aed360f9833d53d489757af2aade80eb18
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-08-22 17:07:54 +0300

    Vedio 01

---
+++ Session 01/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_01
+{
+    // Generic Class
+    internal static class Helper<T>
+    {
+        public static void Print(T X)
+        {
+            Console.WriteLine(X);
+        }
+        // Genaric Method
+        public static void SWAP(ref T x , ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+        // Passing Paramter Value Type by Value
+        //public static void SWAP(ref int x , ref int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+        //public static void SWAP(ref decimal x , ref decimal y)
+        //{
+        //    decimal temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+        //public static void SWAP(ref Point x , ref Point y)
+        //{
+        //    Point temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+    }
+}
--- Session 01/Program.cs
+++ Session 01/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-----------------------------------Vedio 01-----------------------------------
+            //int a = 10, b = 20;
+            //Console.WriteLine($"Before Swap: \na = {a} \nb = {b}");
+            //Helper<int>.SWAP(ref a, ref b);
+            //Console.WriteLine($"After Swap: \na = {a} \nb = {b}");
+
+            //decimal M = 10.10M, L = 20.20M;
+            //Console.WriteLine($"Before Swap: \nM = {M} \nL = {L}");
+            //Helper<decimal>.SWAP(ref M, ref L);
+            //Console.WriteLine($"After Swap: \nM = {M} \nL = {L}");
+
+            //Point P01 = new Point(10, 20);
+            //Point P02 = new Point(100, 200);
+            //Console.WriteLine("Before Swap:");
+            //Console.WriteLine($"P01 => {P01}");
+            //Console.WriteLine($"P02 => {P02}");
+            //Helper<Point>.SWAP(ref P01, ref P02);
+            //Console.WriteLine("After Swap:");
+            //Console.WriteLine($"P01 => {P01}");
+            //Console.WriteLine($"P02 => {P02}");
         }
     }
 }
