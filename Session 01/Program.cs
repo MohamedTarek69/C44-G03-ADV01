b11fadfc3e9680fa5e7be7497f83ccdd83bc9132
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-08-23 15:41:22 +0300

    Q1

--- Session 01/Program.cs
+++ Session 01/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 
+            //Range<int> Range01 = new Range<int>(1, 10);
+            //Console.WriteLine($"Range Length: {Range01.Length()}");
+            //Console.WriteLine($"Is 5 in Range: {Range01.IsInRange(5)}");
+            //Console.WriteLine($"Is 15 in Range: {Range01.IsInRange(15)}");
+
+            //Range<double> Range02 = new Range<double>(1.5, 10.5);
+            //Console.WriteLine($"Range Length: {Range02.Length()}");
+            //Console.WriteLine($"Is 5.5 in Range: {Range02.IsInRange(5.5)}");
+            //Console.WriteLine($"Is 15.5 in Range: {Range02.IsInRange(15.5)}");
+
+            #endregion
         }
     }
 }
---
+++ Session 01/Range.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Session_01
+{
+    internal class Range<T> where T : IComparable<T> , INumber<T>
+    {
+        #region Properties
+        public T Max { get; }
+
+        public T Min { get; }
+
+        #endregion
+
+        #region Constructors
+        public Range(T min, T max) {
+            Max = max;
+            Min = min;
+        }
+
+        #endregion
+
+        #region Methods
+        public bool IsInRange(T value) 
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+        public T Length()
+        {
+            return Max - Min;
+        }
+
+        #endregion
+    }
+
+}
